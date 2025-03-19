e89471d4162b31dcaa3fe9e68ee58f3e152a26c8
choseondong <43941312+choseondong@users.noreply.github.com>
2025-03-19 15:46:40 +0900

    [add] faq 검색기능 추가

--- tests/test_QA.py
+++ tests/test_QA.py
@@ -2,7 +2,7 @@ import pytest
 from pages.qa import QA
 
 class TESTQA:
-    #@pytest.mark.skip(reason="확인 테스트")
+    @pytest.mark.skip(reason="확인 테스트")
     def test_QA(self,driver):
         Qa = QA(driver)
         Qa.QA_assert(driver)
