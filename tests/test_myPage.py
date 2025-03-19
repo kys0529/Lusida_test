03eb08848c402ceb8b5dfbd3f42da3cfc9c93502
YeonSu <rkddustn2519@naver.com>
2025-03-19 17:04:45 +0900

    [ADD] 위시리스트 함수 추가
    - 오류 발생 부분 주석 처리
    - 요소 찾지 못하는 문제 해결하기

--- tests/test_myPage.py
+++ tests/test_myPage.py
@@ -2,11 +2,21 @@ import pytest
 from pages.myPage import MyPage
 
 class TestMyPage:
+    @pytest.mark.skip(reason="테스트 완료!")
     @pytest.mark.parametrize("text", ["주문내역", "쿠폰내역", "적립금내역", "위시리스트", "내가쓴글"])
     def test_open_mypage_hide_menu(self, driver, text):
         try:
             my_page = MyPage(driver)
             my_page.auto_login(driver)   
             my_page.open_mypage_hide_menu(text)
+        except Exception as e:
+            print(f"[❗] {e}")
+    
+    @pytest.mark.skip(reason="미완성")      
+    def test_open_shop_detail_page(self, driver):
+        try:
+            my_page = MyPage(driver)
+            my_page.auto_login(driver)
+            my_page.open_shop_detail_page()
         except Exception as e:
             print(f"[❗] {e}")
