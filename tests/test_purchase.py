33e13a98f1ee8655a553b3bb8739f50edecbbde4
kirvy9 <kirvy@naver.com>
2025-03-20 17:18:11 +0900

    ..

--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -1,16 +1,15 @@
 import time
 import pytest
-from selenium import webdriver
 from pages.purchasepage import PurchasePage
-from pages.loginPage import LoginPage
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from pages.mainPage import MainPage
 from selenium.webdriver.support.ui import WebDriverWait as ws
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.common.keys import Keys
+from pages.autologin import auto
+from selenium.common.exceptions import NoSuchElementException , TimeoutException, ElementClickInterceptedException
 
 def close_alert_if_present(driver):
         try:
@@ -24,56 +23,61 @@ class TestPurchasePage:
     
     def test_purchase(self, driver: WebDriver):
 
-        try:
-            login_page = LoginPage(driver)
-            login_page.open()
+        #try:
+        main_page = MainPage(driver)
+        auto_login = auto(driver)
+        auto_login.auto_login(driver)
 
-            time.sleep(2)
+        # 로그인 페이지(accounts)로 이동했는지 확인
+        wait = ws(driver, 10) #최대 10초까지 기다림
+        wait.until(EC.url_contains("lusida.co.kr")) #URL 검증
+        assert "lusida.co.kr" in driver.current_url #검증
 
-            wait = ws(driver, 10)
-            wait.until(EC.url_contains("https://www.lusida.co.kr/shop/member.html?type=login"))
-            assert "login" in driver.current_url
+        purchase_page = PurchasePage(driver)
+        time.sleep(2)
 
-            login_page.input_password_and_email()
+        #알림창이 나타나면 닫음
+        close_alert_if_present(driver)
 
-            login_btn = driver.find_element(By.CLASS_NAME, "login_btn")
-            login_btn.click()
+        best50_tab = wait.until(EC.presence_of_element_located((By.XPATH, '//a[span[text()="BEST50"]]')))
+        best50_tab.click()
+        driver.save_screenshot("BEST50-이동-성공.jpg")
+        time.sleep(3)
 
-            #알림창이 나타나면 닫음
-            close_alert_if_present(driver)
+        MAX_RETRIES = 5
+        retry_count = 0
 
-            wait = ws(driver, 10)
-            wait.until(EC.url_contains("https://www.lusida.co.kr/index.html"))
+        while retry_count < MAX_RETRIES:
+            try:
+                  
+                #랜덤 상품 선택
+                purchase_page.select_random_product()
 
-            #로그인 후 메인 페이지로 이동 확인
-            wait.until(EC.url_contains("https://www.lusida.co.kr/index.html"))
+                time.sleep(1)
+                print("상품 선택 성공!")
+                break
+        
+            except ElementClickInterceptedException as e:
+                  print("상품을 찾을 수 없음, 재시도 중...({retry_count + 1}/{MAX_RETRIES})")
+                  retry_count += 1
+                  time.sleep(1)
+        
+            except TimeoutException as e:
+                print(f"상품 선택 Timeout, 재시도 중... ({retry_count + 1}/{MAX_RETRIES})")
+                retry_count += 1
+                time.sleep(1)
 
-            time.sleep(10)
+        if retry_count == MAX_RETRIES:
+              pytest.fail("상품 선택 실패: 최대 재시도 횟수 초과")
 
-        except Exception as e:
-            print(f"로그인 중 오류 발생: {e}")
+        #색상과 사이즈 선택
+        purchase_page.select_color_and_size()
 
-        #     purchase_page = PurchasePage(driver)
-            
+        time.sleep(2)
 
-        #     time.sleep()
-        #     best50_menu = driver.find_element(By.CLASS_NAME, "hover_menu")
-        #     actions = ActionChains(driver)
-        #     actions.move_to_element(best50_menu).perform()
+        #구매버튼 클릭
+        purchase_page.click_purchase_button()
 
-        #     time.sleep(2)
-        #     best50_tab = driver.find_element(By.XPATH, "//a[contains(text(), 'BEST50')]")
-        #     best50_tab.click()
+        time.sleep(5)
+        driver.save_screenshot("BEST50-상품상세옵션-구매-성공.jpg")
 
-        #     time.sleep(5)
-        #     assert "shop/shopbrand.html?xcode=011" in driver.current_url, "BEST50의 페이지로 이동하지 않았습니다!"
-            
-        #     driver.save_screenshot("BEST50-이동-성공.jpg")
-
-        # except NoSuchElementException as e:
-        #     driver.save_screenshot('BEST50이동-실패-요소없음.jpg')
-        #     assert False, f"페이지 로드 실패: 페이지를 찾을 수 없습니다."
-
-        # except TimeoutException as e:
-        #     driver.save_screenshot('BEST50이동-실패-시간초과.jpg')
-        #     assert False, f"페이지 로드 실패: 페이지 로드 시간이 초과했습니다."
